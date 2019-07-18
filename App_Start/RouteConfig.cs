c119e2d9ad18d4a82cd3396571cb0ce2f8ad97d2
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2019-07-18 15:48:24 -0400

    formatting

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MonthlyBillsWebApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "MonthlyBills", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
