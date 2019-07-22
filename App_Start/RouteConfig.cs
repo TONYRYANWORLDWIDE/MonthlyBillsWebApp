5ce18d206e6d953d73e76a04832376ec81835038
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2019-07-21 21:37:18 -0400

    adding login page

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace MonthlyBillsWebApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "MonthlyBills", action = "Index", id = UrlParameter.Optional }
+                //defaults: new { controller = "MonthlyBills", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonthlyBillsWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize()
+        {
+            return View();
+        }
+    }
+}
