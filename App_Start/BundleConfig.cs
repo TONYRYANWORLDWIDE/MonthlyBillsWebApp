33a4f5ce2598a16d806dac32cdacda47e0e4c137
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2019-11-27 00:40:27 -0500

    removed superfluous js files

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -7,14 +7,16 @@ namespace IdentitySample
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            "~/Scripts/jquery-ui-1.12.1.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-3.4.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            "~/Scripts/jquery-ui-1.12.1.js"));
+
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
