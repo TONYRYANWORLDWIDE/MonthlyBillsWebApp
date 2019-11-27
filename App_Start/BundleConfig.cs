7388d961522072c63522d4b5f2a8d1d631aa83e4
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2019-11-27 00:33:19 -0500

    slowly getting there

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,13 +8,13 @@ namespace IdentitySample
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.4.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            "~/Scripts/jquery-ui-{version}.js"));
+            "~/Scripts/jquery-ui-1.12.1.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
