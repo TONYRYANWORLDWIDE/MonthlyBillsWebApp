187dbf2acc25d8db78d7a5fbf95bd53452d83c5c
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2019-12-09 15:46:18 -0500

    Time crunch quick fix needed to alter upcoming bills. Currently compares itself to upcoming bills alter and updates value where it finds matches. Will combine functionality to one view in next iteration. User will simply make edit to Upcoming Bills view which will actually update UpcomingBills_alter which will subsequently alter Upcoming BIlls.

--- Controllers/UpcomingBills_AlterController.cs
+++ Controllers/UpcomingBills_AlterController.cs
@@ -14,37 +14,14 @@ namespace MonthlyBillsWebApp.Controllers
     public class UpcomingBills_AlterController : Controller
     {
         private BillsEntities db = new BillsEntities();
-
-        // GET: UpcomingBills_Alter
         public async Task<ActionResult> Index()
         {
             return View(await db.UpcomingBills_Alter.ToListAsync());
         }
-
-        // GET: UpcomingBills_Alter/Details/5
-        public async Task<ActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            UpcomingBills_Alter upcomingBills_Alter = await db.UpcomingBills_Alter.FindAsync(id);
-            if (upcomingBills_Alter == null)
-            {
-                return HttpNotFound();
-            }
-            return View(upcomingBills_Alter);
-        }
-
-        // GET: UpcomingBills_Alter/Create
         public ActionResult Create()
         {
             return View();
         }
-
-        // POST: UpcomingBills_Alter/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "id,TheDate,DayOfWeek,Name,Amount,Type,UserID")] UpcomingBills_Alter upcomingBills_Alter)
@@ -55,11 +32,8 @@ namespace MonthlyBillsWebApp.Controllers
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-
             return View(upcomingBills_Alter);
         }
-
-        // GET: UpcomingBills_Alter/Edit/5
         public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
@@ -73,10 +47,6 @@ namespace MonthlyBillsWebApp.Controllers
             }
             return View(upcomingBills_Alter);
         }
-
-        // POST: UpcomingBills_Alter/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit([Bind(Include = "id,TheDate,DayOfWeek,Name,Amount,Type,UserID")] UpcomingBills_Alter upcomingBills_Alter)
@@ -89,8 +59,6 @@ namespace MonthlyBillsWebApp.Controllers
             }
             return View(upcomingBills_Alter);
         }
-
-        // GET: UpcomingBills_Alter/Delete/5
         public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
@@ -104,8 +72,6 @@ namespace MonthlyBillsWebApp.Controllers
             }
             return View(upcomingBills_Alter);
         }
-
-        // POST: UpcomingBills_Alter/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
@@ -115,7 +81,6 @@ namespace MonthlyBillsWebApp.Controllers
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
